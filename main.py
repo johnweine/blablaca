2175dd53d3e782f8a70bfa24cf504fbebb22bc58
johnweine <johnfilhocz@gmail.com>
2025-04-29 19:55:49 -0300

    primeiro commuit

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+tem_email= ['@' '.com']
+naotem_email = ['$', '%', '&','#',' ']
 usr = []
 senha = []
+
+op= 99
+while op != 0:
+    print('----------MENU---------')
+    print('1-Cadastrar usurario ')
+    print('2-login')
+    print('0-sair')
+
+    op = int(input('digite a opcao desejada '))
+    while True:
+        if op == 1:
+            nome = input('digite seu nome')
+            email = input('digite o email valido')
+            senha = input('digite sua senha')
+
+            if email in ['@', '.com'] and naotem_email not in email:
+                usr.append({
+                    'nome': nome,
+                    'email': email,
+                    'senha': senha
+                }
+                )
+                print('Usuario cadastrado com sucesso')
+                break
+            else:
+                print('email invalido por favor digite novamente email e senha para validação')
+                continue
