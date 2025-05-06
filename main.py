072a658f674451634f5ec100eecb015cc8358504
johnweine <johnfilhocz@gmail.com>
2025-05-06 15:25:41 -0300

    Verificação de email

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ usr_carona=[{
     'data' : '04/06/2025'
 }]
 
-op= 99
+op= '99'
 
-while op != 0:
+while op != '0':
     print('----------MENU---------')
     print('1-Cadastrar usurario ')
     print('2-login')
     print('0-sair')
 
-    op = int(input('digite a opcao desejada '))
+    op = (input('digite a opcao desejada '))
+
+
     while True:
-        if op == 1:
+        if op == '1':
             nome = input('digite seu nome')
             email = input('digite o email valido')
             senha = input('digite sua senha')
@@ -33,7 +35,7 @@ while op != 0:
                     break
 
             if email_exis == False:
-                if '@' in email and '.com' in email and '$' not in email and '%' not in email and '&' not in email and '#' not in email and ' ' not in email:
+                if '@' in email and '.com' in email and '$' not in email and '%' not in email and '&' not in email and '#' not in email and ' ' not in email and '/' not in email and '?' not in email:
                     usr.append({
                         'nome': nome,
                         'email': email,
@@ -45,28 +47,42 @@ while op != 0:
                 else:
                     print('email invalido por favor digite novamente o email e senha para validação')
                     continue
-        if op == 24:
-            print(usr)
-            break
-        if op == 2:
-            while True:
-                op2 = 99
+
+        elif op == '2':#usuario cadastrado
+            op2 = '99'
+            while op2 != '3':
                 email_val = input('digite seu email:')
                 senha_val = input('digite sua senha:')
                 for usuario_cada in usr:
                     if usuario_cada['email'] == email_val and usuario_cada['senha'] == senha_val:
                         print('login realizado com sucesso')
-                        while op2 != 0:
+                        usuario_cada = True
+                        while op2 != '3':
                             print('1-Cadastrar carona')
                             print('2-Pegar carona')
+                            print('3-logout')
                             op2 = int(input('Digite a opção que você deseja: '))
-                            if op2 == 1:
+
+                            if op2 == '1':
                                 local_origin=input('digite o local de origem: ')
                                 local_final=input('digite o destino final: ')
+
+                            elif op2 == '3':
+                                break
+                            else:
+                                print('opção invalida.')
                     else:
                         print('email e senha errado')
 
 
 
 
-
+        elif op == '0':
+            print('Até mais :)')
+            break
+        elif op == '24': #teste
+            print(usr)
+            break
+        else:
+            print('opção invalida! Apenas os números 1, 2 e 3.')
+            break
