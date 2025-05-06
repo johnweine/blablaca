0c9234c453afe9a6892419f68f6b1fa4077dc365
johnweine <johnfilhocz@gmail.com>
2025-05-06 17:14:41 -0300

    Sistema de logout

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 usr = [{'nome': 'john',
         'email': 'john@gmail.com',
-        'senha' : '123'
+        'senha' : '123',
         }]
 usr_carona=[{
     'nome': 'john',
     'local de partida': 'Cajazeiras',
     'destino final': 'Joao pessoa',
-    'data' : '04/06/2025'
+    'data' : '04062025'
+
 }]
 
 op= '99'
+usuario_logado= None
 
 while op != '0':
     print('----------MENU---------')
@@ -20,8 +22,8 @@ while op != '0':
     op = (input('digite a opcao desejada '))
 
 
-    while True:
-        if op == '1':
+    if op == '1':
+        while True:
             nome = input('digite seu nome')
             email = input('digite o email valido')
             senha = input('digite sua senha')
@@ -39,7 +41,8 @@ while op != '0':
                     usr.append({
                         'nome': nome,
                         'email': email,
-                        'senha': senha
+                        'senha': senha,
+                        'logado': False
                             }
                             )
                     print('Usuario cadastrado com sucesso')
@@ -48,41 +51,46 @@ while op != '0':
                     print('email invalido por favor digite novamente o email e senha para validação')
                     continue
 
-        elif op == '2':#usuario cadastrado
-            op2 = '99'
-            while op2 != '3':
-                email_val = input('digite seu email:')
-                senha_val = input('digite sua senha:')
-                for usuario_cada in usr:
-                    if usuario_cada['email'] == email_val and usuario_cada['senha'] == senha_val:
-                        print('login realizado com sucesso')
-                        usuario_cada = True
-                        while op2 != '3':
-                            print('1-Cadastrar carona')
-                            print('2-Pegar carona')
-                            print('3-logout')
-                            op2 = int(input('Digite a opção que você deseja: '))
+    elif op == '2':#usuario cadastrado
+        while True:
+            email = input('digite seu email:')
+            senha = input('digite sua senha:')
 
-                            if op2 == '1':
-                                local_origin=input('digite o local de origem: ')
-                                local_final=input('digite o destino final: ')
+            for usuario_cada in usr:
+                if usuario_cada['email'] == email and usuario_cada['senha'] == senha:
+                    usuario_logado = usuario_cada
+                    print('login realizado com sucesso')
+                    break
+            else:
+                print('email e senha errado')
+                continue
 
-                            elif op2 == '3':
-                                break
-                            else:
-                                print('opção invalida.')
-                    else:
-                        print('email e senha errado')
+        while usuario_logado:
+            print('1-Cadastrar carona')
+            print('2-Pegar carona')
+            print('3-logout')
+            op2 = (input('Digite a opção que você deseja: '))
+
+            if op2 == '1':
+                local_origin=input('digite o local de origem: ')
+                local_final=input('digite o destino final: ')
+            # elif op2 == '2':
+            #     print('')
+
+            elif op2 == '3':
+                print('Até mais :)')
+                usuario_logado = None
+                break
+            else:
+                print('opção invalida.')
 
 
-
-
-        elif op == '0':
-            print('Até mais :)')
-            break
-        elif op == '24': #teste
-            print(usr)
-            break
-        else:
-            print('opção invalida! Apenas os números 1, 2 e 3.')
-            break
+    elif op == '0':
+        print('Até mais :)')
+        break
+    elif op == '24': #teste
+        print(usr)
+        break
+    else:
+        print('opção invalida! Apenas os números 1, 2 e 3.')
+        break
